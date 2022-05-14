b5d034eebaa599675a7159f4110c460a191a8607
VietGau <54618976+vietgau@users.noreply.github.com>
2022-05-14 13:21:48 +0700

    add hook.cpp

---
+++ IATtable/hook.cpp
@@ -0,0 +1,36 @@
+#include "Header.h"
+PSIZE_T FindFunctionAddress(char* funcName) {
+
+	MODULEINFO modInfo;
+	HMODULE hMod = GetModuleHandle(0);
+	GetModuleInformation(GetCurrentProcess(), hMod, &modInfo, sizeof(MODULEINFO));
+	LPBYTE lpAddress = (LPBYTE)modInfo.lpBaseOfDll;
+	PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)lpAddress;
+	PIMAGE_NT_HEADERS pNTHeader = (PIMAGE_NT_HEADERS)(lpAddress + pDosHeader->e_lfanew);
+	PIMAGE_OPTIONAL_HEADER pOptionHeader = (PIMAGE_OPTIONAL_HEADER) & (pNTHeader->OptionalHeader);
+	PIMAGE_IMPORT_DESCRIPTOR pImportDir = PIMAGE_IMPORT_DESCRIPTOR(lpAddress + pOptionHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+	PIMAGE_THUNK_DATA	pOriginalFirstThunk = (PIMAGE_THUNK_DATA)(lpAddress + pImportDir->OriginalFirstThunk);
+	PIMAGE_THUNK_DATA	pFirstThunk = (PIMAGE_THUNK_DATA)(lpAddress + pImportDir->FirstThunk);
+	PSIZE_T				pFunction = nullptr;
+	do {
+		PIMAGE_IMPORT_BY_NAME pName = (PIMAGE_IMPORT_BY_NAME)(lpAddress + pOriginalFirstThunk->u1.AddressOfData);
+		if (!strcmp(funcName, (char*)pName->Name)) {
+			pFunction = (PSIZE_T) & (pFirstThunk->u1.Function);
+			printf(" %s dia chi : 0x%p\n\n", (char*)pName->Name, (void*)(*pFunction));
+			return pFunction;
+		}
+		pFirstThunk++;
+		pOriginalFirstThunk++;
+
+	} while (pOriginalFirstThunk->u1.AddressOfData);
+
+	return 0;
+}
+void RunFunction(char* funcName)
+{
+	PSIZE_T pFunction = FindFunctionAddress(funcName);
+	PSIZE_T pGPA;
+	NewGetProcAddress GPA = (NewGetProcAddress)*pFunction;
+	pGPA = (PSIZE_T)GPA(GetModuleHandle(TEXT("kernel32.dll")), "GetCurrentProcess");
+	printf(" GetCurrentProcess: %p\n\n", pGPA);
+}
